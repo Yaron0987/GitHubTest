ef90aa59e4a2e4158f724c123d6bd35832efc9ef
yaron0987 <yarona@trackmatic.co.za>
2023-01-24 13:21:06 +0200

    Input Form Added

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSubmit : Button = findViewById(R.id.btnSubmit)
+        val car : EditText = findViewById(R.id.idCar)
+        val color : EditText = findViewById(R.id.idColor)
+
+        btnSubmit.setOnClickListener(){
+            if(car.text.isBlank() && color.text.isBlank()){
+                Toast.makeText(this@MainActivity, "Please fill in all the data fields", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this@MainActivity, "Submit!!!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
